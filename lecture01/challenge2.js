6652416cc08abc45dacb58bb5cae1ed4acb4c800
xenyens <xpadilla@ensenada.tecnm.mx>
2022-11-03 12:15:51 -0700

    lecture01 update

--- lecture01/challenge2.js
+++ lecture01/challenge2.js
@@ -1,9 +1,9 @@
 // Coding challenge 2
 
 /**
- * Re-create challenge 1, but this time using ES6 class.
+ * ✅ Re-create challenge 1, but this time using ES6 class.
  *
- * 2. Add a getter called 'SpeedUS' which returns the current speed in mi/h (divide by 1.6)
+ * ✅ 2. Add a getter called 'SpeedUS' which returns the current speed in mi/h (divide by 1.6)
  *
  * 3. Add a setter called 'SpeedUS' which sets the current speed in mi/h (but converts it to km/h before staring the value, by multiplying the input by 1.6)
  *
@@ -12,3 +12,40 @@
  * DATA CAR 1: 'Ford' going at 120 km/h
  * 😘
  */
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`The ${this.make} is current velocity is ${this.speed} km/h`);
+  }
+
+  break() {
+    this.speed -= 5;
+    console.log(`The ${this.make} is current velocity is ${this.speed} km/h`);
+  }
+
+  get SpeedUS() {
+    return this.speed / 1.6;
+  }
+
+  set SpeedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford);
+
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.break();
+console.log(ford.SpeedUS + ' mi/h');
+ford.SpeedUS = 45;
+console.log(ford.SpeedUS + ' mi/h');
