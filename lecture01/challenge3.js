950a4ee7bfc30cd64919b93d7a52bde0b74ba570
xenyens <xpadilla@ensenada.tecnm.mx>
2022-11-04 11:47:00 -0700

    lecture03 challenge3 update

--- lecture01/challenge3.js
+++ lecture01/challenge3.js
@@ -1,7 +1,7 @@
 // Challenge #3
 
 /**
- * 1. Use a constrructor function to implement an Electric Car [called EV] as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property)
+ * 1. Use a constructor function to implement an Electric Car [called EV] as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property)
  *
  * 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo'
  *
@@ -13,3 +13,41 @@
  *
  * 😘😘😘😘
  */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`The ${this.make} is current velocity is ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`The ${this.make} is current velocity is ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Link prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge = this.charge - this.charge * 0.01;
+  console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+};
+
+const tesla = new EV('Tesla', 120, 23);
+
+console.log(tesla);
+tesla.accelerate();
