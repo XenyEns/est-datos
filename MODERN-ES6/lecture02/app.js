9deb54ba163e36ac72ad5394c6d7d801ea87c7aa
xenyens <xpadilla@ensenada.tecnm.mx>
2022-11-17 13:02:24 -0800

    pokemon search

--- MODERN-ES6/lecture02/app.js
+++ MODERN-ES6/lecture02/app.js
@@ -1,6 +1,36 @@
 'use strict';
-
 import { pokeSearch } from './pokeFecht.js';
 
-const pokemon = await pokeSearch();
+async function callPokemon(data) {
+  return await pokeSearch(data.toLowerCase());
+}
+
+const pokemon = await callPokemon('Pikachu');
+
 console.log(pokemon);
+
+const buscar = document.createElement('input');
+const boton = document.createElement('button');
+boton.textContent = 'Buscar Pokemon';
+
+const p = document.createElement('p');
+p.textContent = pokemon.name;
+
+const main = document.getElementById('main');
+
+const imgPokemon = document.createElement('img');
+imgPokemon.src = pokemon.sprites.front_default;
+imgPokemon.alt = pokemon.name;
+
+main.append(buscar);
+main.append(boton);
+main.append(imgPokemon);
+main.append(p);
+
+async function saludar() {
+  const res = await callPokemon(buscar.value);
+  imgPokemon.src = res.sprites.front_default;
+  p.textContent = res.name;
+}
+
+boton.onclick = saludar;
